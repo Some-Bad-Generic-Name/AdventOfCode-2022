a7216e0aba1011e32840d709b851924771341023
Some-Bad-Generic-Name <50808636+Some-Bad-Generic-Name@users.noreply.github.com>
2022-12-10 16:03:45 -0500

    Update puzzle_two.py

--- Day2/puzzle_two.py
+++ Day2/puzzle_two.py
@@ -1,13 +1,42 @@
-def Loss():
-    return 0
+score = 0
 
 
-def Win():
-    return 0
+'''
+Rock		A, X	Worth:	1
+Paper		B, Y 	Worth:	2
+Scissors	C, Z	Worth:	3
+
+Win		6
+Tie		3
+Loss	3
+'''
 
 
-def Tie():
-    return 0
+def Loss(throws):
+    if (throws[0] == "A"):
+        return 3
+    elif (throws[0] == "B"):
+        return 1
+    else:
+        return 2
+
+
+def Win(throws):
+    if (throws[0] == "A"):
+        return 2 + 6
+    elif (throws[0] == "B"):
+        return 3 + 6
+    else:
+        return 1 + 6
+
+
+def Tie(throws):
+    if (throws[0] == "A"):
+        return 1 + 3
+    elif (throws[0] == "B"):
+        return 2 + 3
+    else:
+        return 3 + 3
 
 
 try:
@@ -19,4 +48,9 @@ except:
 for items in input_file:
     throws = items.replace(" ", "")
     if (throws[1] == "X"):
-        Loss()
+        score += Loss(throws)
+    if (throws[1] == "Y"):
+        score += Tie(throws)
+    if (throws[1] == "Z"):
+        score += Win(throws)
+print(score)
